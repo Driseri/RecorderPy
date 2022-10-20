f9741ef2e0fe612fcfd68d8b18f8ce8cb2829680
Driseri <52747544+Driseri@users.noreply.github.com>
2022-10-20 11:57:09 +0300

    Для проверки потока

--- rtspStream.py
+++ rtspStream.py
@@ -1,33 +1,42 @@
 import cv2
 import time
 
-time1 = time.perf_counter()
-#'rtsp://172.18.191.72:554/Streaming/Channels/1'
-try:
-    cap = cv2.VideoCapture('rtsp://172.18.191.84:554/live/0/MAIN')
-except:
-    print('\n\n\n\nНе подключился к камере\n\n\n\n')
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
-frame_size = (frame_width,frame_height)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-fps = 35
-out = cv2.VideoWriter('output.', cv2.VideoWriter_fourcc('M','J','P','G'),fps, frame_size)
+# time1 = time.perf_counter()
+# #'rtsp://172.18.191.72:554/Streaming/Channels/1'
+# try:
+#     cap = cv2.VideoCapture('rtsp://172.18.191.84:554/live/0/MAIN')
+# except:
+#     print('\n\n\n\nНе подключился к камере\n\n\n\n')
+# frame_width = int(cap.get(3))
+# frame_height = int(cap.get(4))
+# frame_size = (frame_width,frame_height)
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# fps = 35
+# out = cv2.VideoWriter('output.', cv2.VideoWriter_fourcc('M','J','P','G'),fps, frame_size)
+#
+# while(cap.isOpened()):
+#     time2 = time.perf_counter()
+#     ret, frame = cap.read()
+#     if ret == True and (time2-time1) < 2:
+#         # write the flipped frame
+#         out.write(frame)
+#
+#         cv2.imshow('frame',frame)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#     else:
+#         break
+#
+# # Release everything if job is finished
+# cap.release()
+# out.release()
+# cv2.destroyAllWindows()
 
-while(cap.isOpened()):
-    time2 = time.perf_counter()
-    ret, frame = cap.read()
-    if ret == True and (time2-time1) < 2:
-        # write the flipped frame
-        out.write(frame)
 
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
-        break
+vcap = cv2.VideoCapture("rtsp://172.18.191.85:554/Streaming/Channels/1")
 
-# Release everything if job is finished
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+while(1):
+
+    ret, frame = vcap.read()
+    cv2.imshow('VIDEO', frame)
+    cv2.waitKey(1)

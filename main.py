6e82e103d263d19711e8afafec970918624c86b0
Ященко Даниил <dsyaschenko@edu.hse.ru>
2022-11-02 00:36:13 +0300

    Небольшой редакт для красоты

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
-# This Python file uses the following encoding: utf-8
 import sys
-from AppCore import AppCore
-from AppCore import VideoModel
-from AppCore import Connector
-
 from pathlib import Path
 
-from PySide2.QtCore import Qt, QUrl, QStringListModel
+from PySide2.QtCore import QStringListModel
 from PySide2.QtGui import QGuiApplication
 from PySide2.QtQml import QQmlApplicationEngine
 
-
+from AppCore import AppCore
+from AppCore import Connector
+from AppCore import VideoModel
 
 if __name__ == "__main__":
-    #Подгрузка ядра приложения
+    # Подгрузка ядра приложения
     connector = Connector()
     appCore = AppCore(connector)
     videoModel = VideoModel(connector)
 
-    #Подгрузка данных камер с API
+    # Подгрузка данных камер с API
     data = appCore.getCameras()
-#    models = CamerModel(data)
+    #    models = CamerModel(data)
 
-    #Запуск приложения
+    # Запуск приложения
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
     qml_file = Path(__file__).resolve().parent / "main.qml"
 
-    #Подсчет свободного места на диске
+    # Подсчет свободного места на диске
     freeSpace = appCore.getFreeSpace()
-    engine.rootContext().setContextProperty("freeSpace",freeSpace)
+    engine.rootContext().setContextProperty("freeSpace", freeSpace)
 
-    #Подключение ядра к QML
+    # Подключение ядра к QML
     engine.rootContext().setContextProperty("appCore", appCore)
 
     #
@@ -39,7 +36,6 @@ if __name__ == "__main__":
     rooms_model = QStringListModel()
     rooms_model.setStringList(rooms)
     engine.rootContext().setContextProperty("listModel", rooms_model)
-
 
     engine.rootContext().setContextProperty("videoModel", videoModel)
 
